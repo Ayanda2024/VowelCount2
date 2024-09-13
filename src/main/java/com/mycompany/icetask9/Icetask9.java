037cf675192673889a01d65371b887addcf07458
RC_Student_lab <RC_Student_lab@10.0.0.15>
2024-09-13 17:19:22 +0000

    First commit

---
+++ src/main/java/com/mycompany/icetask9/Icetask9.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask9;
+import java.util.Scanner;
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Icetask9{
+
+    public static void main(String[] args) {
+        //Declarations
+        String Sentence;
+        String name;
+        //creating an object for the Scanner
+        Scanner sc=new Scanner(System.in);
+        
+        //Prompting the user
+        System.out.print("Enter a sentence");
+        Sentence=sc.next();
+        int countVowels=countVowels(Sentence);
+        int vowelCount = 0;
+        System.out.println("Number of vowels:"+vowelCount);
+        
+        
+    }
+    public static int countVowels(String Sentence){
+        int count=0;
+        Sentence=Sentence.toLowerCase();
+        
+        for(int i=0;i<Sentence.length();i++){
+            char ch=Sentence.charAt(i);
+            if ("aeiou".indexOf(ch)!=-1){
+                count++;
+            }
+            
+            
+        }
+        return count;
+        
+    }
+}
